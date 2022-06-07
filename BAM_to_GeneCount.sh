0d12a47585a8691587ddf1fcd9507bd647a6341f
Bao Le <lebao1213@gmail.com>
2022-06-07 10:03:19 -0400

    Update BAM_to_GeneCount.sh

--- BAM_to_GeneCount.sh
+++ BAM_to_GeneCount.sh
@@ -4,7 +4,7 @@
 ## Reference GTF file is downloaded from ensembl.org https://nov2020.archive.ensembl.org/Mus_musculus/Info/Index (Newer version can be acquired) and stored on local drive
 ## Important to note that -s 2 is defined by the NEB Ultra II RNA Library Prep Kit which generate in a reverse strand format.  Other kits MAY not be the same.  0 is for unstranded, 1 is stranded, and 2 is reversed stranded.
 
-featureCounts -T $(nproc) -p -s 2 -a "/mnt/c/ref/Mouse/Mus_musculus.GRCm38.100.gtf -o genecount.txt ./bam/*.bam
+featureCounts -T $(nproc) -p -s 2 -a "/mnt/c/ref/Mouse/Mus_musculus.GRCm38.100.gtf" -o genecount.txt ./bam/*.bam
 
 ## featureCounts
 ## Liao Y, Smyth GK, Shi W. featureCounts: an efficient general purpose program for assigning sequence reads to genomic features. 
