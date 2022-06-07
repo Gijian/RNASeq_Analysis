55bd224c9d4d2702a0c4b41b604c2fdf76854545
Bao Le <lebao1213@gmail.com>
2022-06-07 12:32:59 -0400

    Update DGE_Analysis_DESeq2.R

--- DGE_Analysis_DESeq2.R
+++ DGE_Analysis_DESeq2.R
@@ -5,24 +5,21 @@
 ## install.packages("DESeq2")
 ## BiocManager::install("ensembldb")
 library(DESeq2)
-library(ensembldb)
 
 ## Create Ensembl annotation object SQLite for reference gene using same reference GTF as featureCounts for consistency
-# ensDbFromGtf("C:/ref/Mouse/Mus_musculus.GRCm38.100.gtf") ## only run of need to create sqlite file
+# ensembldb::ensDbFromGtf("C:/ref/Mouse/Mus_musculus.GRCm38.100.gtf") ## only run of need to create sqlite file for gene symbol mapping later
 
-ensdb<-EnsDb("C:/ref/Mouse/Mus_musculus.GRCm38.100.sqlite") ## Load sqlite
+ensdb<-ensembldb::EnsDb("C:/ref/Mouse/Mus_musculus.GRCm38.100.sqlite") ## Load sqlite
 
 d <- as.data.frame(data.table::fread("gene_count.txt")) ## data.table::fread load large file much faster
 d <- d[,-2:-6]  ## Remove unnecessary column from gene count file for DESeq2 DGE
 colnames(d) <- gsub(".bam", "", colnames(d))  ## Remove .bam from sample name
-d <- d[rowMeans(d[-1] > 5,] ## Remove reads of gene that average to be below 5
-d$symbol <- mapIds(ensdb,d$Geneid,"GENENAME","GENEID") ## Replace ensembl GENEID with more familiar gene symbol
-d$Geneid <- NULL  ## Once the gene symbols are annotated the GENEID is no longer necessary (can be kepted if have other purpose)
+d <- d[rowMeans(d[-1]) > 5,] ## Remove reads of gene that average to be below 5
 d <- tibble::remove_rownames(d)  ## Remove rownames (if it exist to be replace with symbol)
-d <- tibble::column_to_rownames(d,var = "symbol") ## This will place the symbol column into rownames
+d <- tibble::column_to_rownames(d,var = "Geneid") ## This will place the symbol column into rownames
 
 ## This part will creates the coldata for the DGE
-md <- data.frame(sample = colnames(d), dex = c(rep("exp",3),rep("ctrl",3))) ## This will create a data.frame with 2 columns: Sample ID, and comparison group.  The order can be change but for my purposes, the first 3 are experimentals and the last 3 are controls.
+md <- data.frame(sample = colnames(d), dex = c(rep("treatment",3),rep("control",3))) ## This will create a data.frame with 2 columns: Sample ID, and comparison group.  The order can be change but for my purposes, the first 3 are experimentals and the last 3 are controls.
 
 ## Creating a dds object from everything we created
 dds <- DESeqDataSetFromMatrix(countData = d,
@@ -36,19 +33,27 @@ plotPCA(vsd,"dex") ## Examine sample variance of treatment and control group
 dds <- DESeq(dds) ## Run default setting for differential expression analysis.  This function negates the need to run estimateSizeFactors, estimateDispersions, and nbinoWaldTest individually.
 
 res <- as.data.frame(results(dds, contrast = c("dex","treatment","control")))  ## Create differential gene expression results from treatment_vs_control
-                
+res$symbol <-  mapIds(x = ensembldb::EnsDb("C:/ref/Mouse/Mus_musculus.GRCm38.100.sqlite"), ## Mapping gene symbol from ensembl id using the same gtf files done from featureCounts
+                      keys = row.names(res),
+                      column = "GENENAME",
+                      keytype = "GENEID") ## Replace ensembl GENEID with more familiar gene symbol
+
 ## Alternatively, I also used l2fcshrinkage to account for LFC of genes with low expression or high variation, which is common in sample size less than 5 per groups.
 ## I went with apeglm (Approximate Posterior Estimation for General Linear Model) approach to shrinkage which requires the installation of the "apeglm" package
 
 # install.packages("apeglm")
 
-res.ape <- as.data.frame(lfcShrink(dds = dds, coef = 2, type = "apeglm", apeMethod = "general")  
+res.ape <- as.data.frame(lfcShrink(dds = dds, coef = 2, type = "apeglm", apeMethod = "general"))
+res.ape$symbol <- mapIds(x = ensembldb::EnsDb("C:/ref/Mouse/Mus_musculus.GRCm38.100.sqlite"),
+                      keys = row.names(res),
+                      column = "GENENAME",
+                      keytype = "GENEID") 
 ## apeMethod = "general" is a bit slower but I prefer it.  The default can still be used.
 ## coef = 2 is used because it is the index designated for "treatment_vs_control" when you check with resultsNames(dds)
 
 ## Write out the DGE results as csv files for manual examination in excel examination and data visualization 
 write.csv(res, "Results_DGE_of_Treatment_vs_Control.csv")
-write.csv(res, "Results_DGE_of_Treatment_vs_Control_shrinkage.csv")
+write.csv(res.ape, "Results_DGE_of_Treatment_vs_Control_shrinkage.csv")
                          
 ## DESeq2 normalized counts can be extracted out below for data visualization as well but I prefer TPM calculation.  
 write.csv(as.data.frame(counts(dds,normalized = T)),"DESeq2_normalized_gene_counts.csv")
